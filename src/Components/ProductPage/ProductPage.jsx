e7125453888c2a0af0dd6c8fc3fd96af6381598c
Erik Akerman <erikakerman@gmail.com>
2024-11-13 12:22:14 +0100

    title, price, thumbnail

--- src/Components/ProductPage/ProductPage.jsx
+++ src/Components/ProductPage/ProductPage.jsx
@@ -16,6 +16,25 @@ export default function ProductPage() {
     return null;
   }
 
+  const ProductTitle = ({ title }) => {
+    return <h1 className="product-title">{title}</h1>;
+  };
+
+  const ProductPrice = ({ price }) => {
+    return <div className="product-price">{price},00 kr.</div>;
+  };
+
+  const ColorSelection = ({ imageUrl }) => {
+    return (
+      <div className="color-selection">
+        <p className="color-label">FÄRG: Svart</p>
+        <div className="color-thumbnail">
+          <img src={imageUrl} alt="Black color thumbnail" />
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="product-page">
       {product !== null ? (
@@ -23,7 +42,11 @@ export default function ProductPage() {
           <div className="left-side">
             <img className="pimage" src={product.image} alt="No image" />
           </div>
-          <div className="right-side">Right side content here</div>
+          <div className="right-side">
+            <ProductTitle title={product.title} />
+            <ProductPrice price={product.price} />
+            <ColorSelection imageUrl={product.image} />
+          </div>
         </div>
       ) : (
         /*
