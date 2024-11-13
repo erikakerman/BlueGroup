b57703bc667149b100672e466beead2aff73d74b
gitnet <gitnet51@gmail.com>
2024-11-13 09:37:01 +0100

    Created json file and edit on categories page

--- src/Components/CategoryPage.jsx
+++ src/Components/CategoryPage.jsx
@@ -1,20 +1,22 @@
 import { Link } from 'react-router-dom'
 
 export default function CategoryPage({ products }) {
-
     return (
         <div>
-            {products.map(product => {
+            {
+              products.map(product => {
                 return <div key={product.id} className='card'>
-
                     <div>
-                        <img className='pimage' src={product.image} alt="No image" />
+                        <Link to={'product/' + product.id}>
+                            <img className='pimage' src={product.image} alt="No image" />
+                        </Link>
                     </div>
                     <div>
                         <Link to={'product/' + product.id}>
                             <div>{product.title}</div>
                         </Link>
                     </div>
+                    
                 </div>
 
             })}
