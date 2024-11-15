a300e85c4f39658c2ebeecc1992ebf956371f821
Erik Akerman <erikakerman@gmail.com>
2024-11-15 13:44:04 +0100

    Landing page design

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,12 @@
+import "./Home.css";
+
 export default function Home() {
   return (
-    <h1>Welcome to BlueGroup</h1>
-  )
+    <div className="homeContainer">
+      <div className="titleContainer">
+        <h1>Welcome to BlueGroup</h1>
+        <h2>Where fashion meets quality</h2>
+      </div>
+    </div>
+  );
 }
