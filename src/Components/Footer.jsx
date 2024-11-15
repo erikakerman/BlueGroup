2eaa2d05eb834427453cee4619f0921be84cf052
erkanguler <erkan007@gmail.com>
2024-11-15 13:46:44 +0100

    Implemented footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div>
+            <hr />
+            <img src="/footer.png" alt="No image" />
+        </div>
+    )
+}
